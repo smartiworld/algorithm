e1937e84d95c038b10dacdc381951bab6a28d228
smartiworld <smartiworld@163.com>
2022-04-15 23:05:29 +0800

    图的拓扑排序

---
+++ src/com/iworld/algorithm/graph/GraphTopologySort.java
@@ -0,0 +1,47 @@
+package com.iworld.algorithm.graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * @author gq.cai
+ * @version 1.0
+ * @description 图的拓扑排序
+ * @date 2022/4/15 22:41
+ */
+public class GraphTopologySort {
+    
+    public static List<Graph.Node<String>> topologySort(Graph<String> graph) {
+        if (graph == null || graph.nodeMap == null) {
+            return null;
+        }
+        List<Graph.Node<String>> result = new ArrayList<>();
+        // 存放入度为0的点 宽度方式---- 也可以栈 深度方式
+        Queue<Graph.Node<String>> queue = new LinkedList<>();
+        // 记录图中点的入度
+        Map<Graph.Node<String>, Integer> inCounter = new HashMap<>();
+        // 维护所有点的入度  将入度为0的点放队列
+        for (Graph.Node<String> node : graph.nodeMap.values()) {
+            inCounter.put(node, node.in);
+            if (node.in == 0) {
+                queue.offer(node);
+            }
+        }
+        while (!queue.isEmpty()) {
+            Graph.Node<String> poll = queue.poll();
+            result.add(poll);
+            for (Graph.Node<String> next : poll.nexts) {
+                Integer in = inCounter.get(next);
+                inCounter.put(next, --in);
+                if (in == 0) {
+                    queue.add(next);
+                }
+            }
+        }
+        return result;
+    }
+}
