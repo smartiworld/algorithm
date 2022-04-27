3d734fc2859a79b7185fddb9edf753f280bc237f
smartiworld <smartiworld@163.com>
2022-04-27 15:19:34 +0800

    满二叉树

---
+++ src/com/iworld/algorithm/tree/IsFullBinaryTree.java
@@ -0,0 +1,71 @@
+package com.iworld.algorithm.tree;
+
+/**
+ * @author gq.cai
+ * @version 1.0
+ * @description 是否为满二叉树
+ * @date 2022/4/27 10:08
+ */
+public class IsFullBinaryTree {
+
+    public static boolean isFull(CommonBinaryTree<Integer> head) {
+        if (head == null) {
+            return true;
+        }
+        TreeInfo treeInfo = process(head);
+        // 满二叉树为2的高度次方-1 == 树的节点数量
+        return (Math.pow(2, treeInfo.height) - 1) == treeInfo.count;
+    }
+    
+    private static TreeInfo process(CommonBinaryTree<Integer> head) {
+        if (head == null) {
+            return new TreeInfo(0, 0);
+        }
+        TreeInfo leftTreeInfo = process(head.left);
+        TreeInfo rightTreeInfo = process(head.right);
+        int height = Math.max(leftTreeInfo.height, rightTreeInfo.height) + 1;
+        int count = leftTreeInfo.count + 1 + rightTreeInfo.count;
+        return new TreeInfo(height, count);
+    }
+    
+    public static void main(String[] args) {
+        CommonBinaryTree<Integer> root = new CommonBinaryTree<Integer>(1);
+        CommonBinaryTree<Integer> node2 = new CommonBinaryTree<Integer>(2);
+        CommonBinaryTree<Integer> node3 = new CommonBinaryTree<Integer>(3);
+        root.left = node2;
+        root.right = node3;
+        CommonBinaryTree<Integer> node4 = new CommonBinaryTree<Integer>(4);
+        CommonBinaryTree<Integer> node5 = new CommonBinaryTree<Integer>(5);
+        node2.left = node4;
+        //node2.right = node5;
+        CommonBinaryTree<Integer> node6 = new CommonBinaryTree<Integer>(6);
+        CommonBinaryTree<Integer> node7 = new CommonBinaryTree<Integer>(7);
+        //node3.left = node6;
+        //node3.right = node7;
+//        CommonBinaryTree<Integer> node9 = new CommonBinaryTree<Integer>(9);
+//        CommonBinaryTree<Integer> node10 = new CommonBinaryTree<Integer>(10);
+//        node4.right = node9;
+//        node5.right = node10;
+        System.out.println(isFull(root));
+    }
+    
+    static class TreeInfo{
+        // 树的高度
+        int height;
+        // 节点数量
+        int count;
+        
+        TreeInfo(int height, int count) {
+            this.height = height;
+            this.count = count;
+        }
+    
+        @Override
+        public String toString() {
+            return "TreeInfo{" +
+                    "height=" + height +
+                    ", count=" + count +
+                    '}';
+        }
+    }
+}
