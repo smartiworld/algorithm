247ec76871952a349b68d743271fa8a2adc82d6a
smartiworld <smartiworld@163.com>
2022-06-29 16:34:41 +0800

    最长不重复字符串 添加注释

--- src/com/iworld/algorithm/strings/SubStrWithoutRepeating.java
+++ src/com/iworld/algorithm/strings/SubStrWithoutRepeating.java
@@ -3,7 +3,7 @@ package com.iworld.algorithm.strings;
 /**
  * @author gq.cai
  * @version 1.0
- * @description 3. Longest Substring Without Repeating Characters
+ * @description 3. Longest Substring Without Repeating Characters    Medium
  * Given a string s, find the length of the longest substring without repeating characters.
  *
  *
@@ -40,13 +40,17 @@ public class SubStrWithoutRepeating {
             return 0;
         }
         int max = 0;
+        // 下标为当前字符ascii value字符在串中下标
         Integer[] indexCache = new Integer[256];
         for (int l = 0, r = 0; r <= s.length(); r++) {
+            // 当前已经来到字符串最后位置 或者缓存中已经存在当前字符
             if (r == s.length() || indexCache[s.charAt(r)] != null) {
+                // 以l开始位置最长不重复结算
                 max = Math.max(max, r - l);
                 if (r == s.length()) {
                     break;
                 }
+                // 最小位置不能小于前出现位置的下一个位置 abba
                 l = Math.max(l, indexCache[s.charAt(r)] + 1);
             }
             indexCache[s.charAt(r)] = r;
@@ -58,6 +62,7 @@ public class SubStrWithoutRepeating {
         int result = 0;
         int[] cache = new int[256];
         for (int i = 0, j = 0; i < s.length(); i++) {
+            // 存在现同字符则跳左边界
             j = (cache[s.charAt(i)] > 0) ? Math.max(j, cache[s.charAt(i)]) : j;
             cache[s.charAt(i)] = i + 1;
             result = Math.max(result, i - j + 1);
@@ -65,14 +70,19 @@ public class SubStrWithoutRepeating {
         return result;
     }
     
+    /**
+     * 使用左右边界
+     * @param s
+     * @return
+     */
     public int lengthOfLongestSubstring3(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
         int l = 0, r = 0, longest = 0;
+        // ascii做下标 value为字符出现位置
         int[] chars = new int[95];
         int offset = 32;
-        
         while (r < s.length()) {
             int c = s.charAt(r);
             l = Math.max(chars[c - offset], l);
@@ -89,5 +99,6 @@ public class SubStrWithoutRepeating {
         String s = "abba";
         System.out.println(subStrWithoutRepeating.lengthOfLongestSubstring(s));
         System.out.println(subStrWithoutRepeating.lengthOfLongestSubstring2(s));
+        System.out.println(subStrWithoutRepeating.lengthOfLongestSubstring3(s));
     }
 }
