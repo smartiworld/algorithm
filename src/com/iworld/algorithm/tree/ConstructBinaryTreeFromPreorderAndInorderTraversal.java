f4b1affc6b4e9d2d8ef26b3a9729c14bf67a9951
smartiworld <smartiworld@163.com>
2022-08-22 13:24:01 +0800

    根据先序和中序遍历构造一棵二叉树 添加注释

--- src/com/iworld/algorithm/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/com/iworld/algorithm/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -44,18 +44,36 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
         return process(preorder, 0, n - 1, inorder, 0, m - 1, inOrderIndexMap);
     }
     
+    /**
+     *
+     * @param preorder
+     * @param s1        先序构造开始位置
+     * @param e1        先序构造结束位置
+     * @param inorder
+     * @param s2        中序构造开始位置
+     * @param e2        中序构造结束位置
+     * @param inOrderIndexMap  中序各位置元素下标
+     * @return
+     */
     private TreeNode process(int[] preorder, int s1, int e1, int[] inorder, int s2, int e2, Map<Integer, Integer> inOrderIndexMap) {
         if (s1 > e1) {
             return null;
         }
+        // 根据先序遍历的开始位置先构造出一颗子树的头
         TreeNode head = new TreeNode(preorder[s1]);
         if (s1 == e1) {
+            // 此时先序和后续同节点
             return head;
         }
+        // 找出当前头在中序遍历中的位置
+        // 中序遍历s2~headInOrderIndex-1 即为当前头的左树
+        // 先序遍历s1+1~s1+count(中序遍历左树元素个数)为当前头的左树
+        // 在中序遍历headInOrderIndex + 1~e2 中当前树的右树
+        // 先序遍历中s1+count(中序遍历左树元素个数)+1~e1 为当前树的右树
         int headInOrderIndex = inOrderIndexMap.get(preorder[s1]);
         int count = headInOrderIndex - s2;
         head.left = process(preorder, s1 + 1, count + s1, inorder, s2, headInOrderIndex - 1, inOrderIndexMap);
-        head.right = process(preorder, headInOrderIndex - s2 + s1 + 1, e1, inorder, headInOrderIndex + 1, e2, inOrderIndexMap);
+        head.right = process(preorder, count + s1 + 1, e1, inorder, headInOrderIndex + 1, e2, inOrderIndexMap);
         return head;
     }
     
