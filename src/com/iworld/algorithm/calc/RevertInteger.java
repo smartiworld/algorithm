7766807eb47911c40019ef558e99b46b07444ac4
smartiworld <smartiworld@163.com>
2022-08-30 14:55:59 +0800

    integer反转 修改class name

--- src/com/iworld/algorithm/calc/RevertInteger.java
+++ src/com/iworld/algorithm/calc/RevertInteger.java
@@ -3,10 +3,35 @@ package com.iworld.algorithm.calc;
 /**
  * @author gq.cai
  * @version 1.0
- * @description
+ * @description 7. Reverse Integer
+ * Medium
+ * 8166
+ * 10529
+ * Given a signed 32-bit integer x, return x with its digits reversed.
+ * If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ *
+ * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+ *
+ * Example 1:
+ *
+ * Input: x = 123
+ * Output: 321
+ * Example 2:
+ *
+ * Input: x = -123
+ * Output: -321
+ * Example 3:
+ *
+ * Input: x = 120
+ * Output: 21
+ *
+ * Constraints:
+ *
+ * -231 <= x <= 231 - 1
+ * https://leetcode.com/problems/reverse-integer/
  * @date 2022/5/29 23:25
  */
-public class IntegerRevert {
+public class RevertInteger {
     
     public int reverse(int x) {
         int result = 0;
@@ -103,7 +128,7 @@ public class IntegerRevert {
     }
     
     public static void main(String[] args) {
-        IntegerRevert integerRevert = new IntegerRevert();
+        RevertInteger integerRevert = new RevertInteger();
         int a = 2147483647;//2143847412
         System.out.println(integerRevert.reverse(-2147483412));
         System.out.println(integerRevert.reverse2(1534236469));
