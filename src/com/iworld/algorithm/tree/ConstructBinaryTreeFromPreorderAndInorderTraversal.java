b2a305ccd2183cfca22b46a9f21aae0700de2fb2
smartiworld <smartiworld@163.com>
2023-04-21 14:49:07 +0800

    105.给定二叉树的先序和中序遍历构造二叉树

--- src/com/iworld/algorithm/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/com/iworld/algorithm/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -1,7 +1,10 @@
 package com.iworld.algorithm.tree;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * @author gq.cai
@@ -41,21 +44,21 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
             inOrderIndexMap.put(inorder[i], i);
         }
         int n = preorder.length;
-        return process(preorder, 0, n - 1, inorder, 0, m - 1, inOrderIndexMap);
+        return process(preorder, 0, n - 1, 0, m - 1, inOrderIndexMap);
     }
     
     /**
-     *
+     * 先序遍历头在中序遍历中 头左边为二叉树的左子树 头右边数字为二叉树的右树
+     * 1.拿出先序遍历的头  在中序遍历中找出头在中序遍历的位置
      * @param preorder
      * @param s1        先序构造开始位置
      * @param e1        先序构造结束位置
-     * @param inorder
      * @param s2        中序构造开始位置
      * @param e2        中序构造结束位置
      * @param inOrderIndexMap  中序各位置元素下标
      * @return
      */
-    private TreeNode process(int[] preorder, int s1, int e1, int[] inorder, int s2, int e2, Map<Integer, Integer> inOrderIndexMap) {
+    private TreeNode process(int[] preorder, int s1, int e1, int s2, int e2, Map<Integer, Integer> inOrderIndexMap) {
         if (s1 > e1) {
             return null;
         }
@@ -71,15 +74,80 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
         // 在中序遍历headInOrderIndex + 1~e2 中当前树的右树
         // 先序遍历中s1+count(中序遍历左树元素个数)+1~e1 为当前树的右树
         int headInOrderIndex = inOrderIndexMap.get(preorder[s1]);
+        // 左树节点数量
         int count = headInOrderIndex - s2;
-        head.left = process(preorder, s1 + 1, count + s1, inorder, s2, headInOrderIndex - 1, inOrderIndexMap);
-        head.right = process(preorder, count + s1 + 1, e1, inorder, headInOrderIndex + 1, e2, inOrderIndexMap);
+        // 先序数组中左树结束位置count + s1 中序数组左树终止位置headInOrderIndex - 1
+        head.left = process(preorder, s1 + 1, count + s1, s2, headInOrderIndex - 1, inOrderIndexMap);
+        // 先序数组左树剩余位置为右树节点
+        head.right = process(preorder, count + s1 + 1, e1, headInOrderIndex + 1, e2, inOrderIndexMap);
         return head;
     }
     
     public static void main(String[] args) {
         int[] preorder = {3,9,20,15,7}, inorder = {9,3,15,20,7};
         ConstructBinaryTreeFromPreorderAndInorderTraversal constructBinaryTreeFromPreorderAndInorderTraversal = new ConstructBinaryTreeFromPreorderAndInorderTraversal();
-        constructBinaryTreeFromPreorderAndInorderTraversal.buildTree(preorder, inorder);
+        TreeNode treeNode = constructBinaryTreeFromPreorderAndInorderTraversal.buildTree(preorder, inorder);
+        List<Integer> preOrders = preOrder(treeNode);
+        System.out.println(preOrders);
+        List<Integer> inOrders = inOrder(treeNode);
+        System.out.println(inOrders);
+        List<Integer> postOrders = postOrder(treeNode);
+        System.out.println(postOrders);
+    }
+    
+    private static List<Integer> preOrder(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode treeNode = stack.pop();
+            result.add(treeNode.val);
+            TreeNode right = treeNode.right;
+            if (right != null) {
+                stack.push(right);
+            }
+            TreeNode left = treeNode.left;
+            if (left != null) {
+                stack.push(left);
+            }
+        }
+        return result;
+    }
+    
+    private static List<Integer> inOrder(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode pop = stack.pop();
+                result.add(pop.val);
+                cur = pop.right;
+            }
+        }
+        return result;
+    }
+    
+    public static List<Integer> postOrder(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        TreeNode prePop = null;
+        while (!stack.isEmpty()) {
+            TreeNode peek = stack.peek();
+            if (peek.left != null && prePop != peek.left && prePop != peek.right) {
+                stack.push(peek.left);
+            } else if (peek.right != null && prePop != peek.right) {
+                stack.push(peek.right);
+            } else {
+                TreeNode pop = stack.pop();
+                result.add(pop.val);
+                prePop = pop;
+            }
+        }
+        return result;
     }
 }
