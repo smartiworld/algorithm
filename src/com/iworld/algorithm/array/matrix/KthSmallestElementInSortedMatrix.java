0bf6aa90dda3057e41618e7f4d2636245e8e2939
smartiworld <smartiworld@163.com>
2023-06-03 16:43:13 +0800

    378.有序矩阵中查找第k小数

--- src/com/iworld/algorithm/array/matrix/KthSmallestElementInSortedMatrix.java
+++ src/com/iworld/algorithm/array/matrix/KthSmallestElementInSortedMatrix.java
@@ -102,6 +102,98 @@ public class KthSmallestElementInSortedMatrix {
         }
     }
     
+    public static int kthSmallest2(int[][] matrix, int k) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int left = matrix[0][0];
+        int right = matrix[row - 1][col - 1];
+        int ans = 0;
+        while (left <= right) {
+            // 计算中间数
+            int mid = left + ((right - left) >> 1);
+            // 在矩阵中找出小于当前mid的最大数 和在矩阵中的排名
+            Info info = noMoreNum(matrix, mid);
+            if (info.num < k) {
+                // 中间数排名小于k 推大中间数
+                left = mid + 1;
+            } else {
+                // 中间数排名大于等于k
+                ans = info.near;
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+    
+    public static int kthSmallest3(int[][] matrix, int k) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int left = matrix[0][0];
+        int right = matrix[row - 1][col - 1];
+        int ans = 0;
+        while (left <= right) {
+            // 计算中间数
+            int mid = left + ((right - left) >> 1);
+            // 在矩阵中找出小于当前mid的最大数 和在矩阵中的排名
+            Info info = noMoreNum(matrix, mid);
+            if (info.num < k) {
+                // 中间数排名小于k 推大中间数
+                left = mid + 1;
+            } else if(info.num > k) {
+                // 中间数排名大于等于k
+                ans = info.near;
+                right = mid - 1;
+            } else {
+                ans = info.near;
+                break;
+            }
+        }
+        return ans;
+    }
+    
+    public static class Info {
+        // 矩阵中的值
+        public int near;
+        // 当前值在矩阵中第num小
+        public int num;
+        
+        public Info(int n1, int n2) {
+            near = n1;
+            num = n2;
+        }
+    }
+    
+    /**
+     * 找出最接近小于value的位置值 并且返回当前值在矩阵中第几小
+     * @param matrix
+     * @param value
+     * @return
+     */
+    public static Info noMoreNum(int[][] matrix, int value) {
+        // 最接近小于value的值
+        int near = Integer.MIN_VALUE;
+        // 第num小
+        int num = 0;
+        int rowEnd = matrix.length;
+        int colEnd = matrix[0].length;
+        int row = 0;
+        int col = colEnd - 1;
+        while (row < rowEnd && col >= 0) {
+            if (matrix[row][col] <= value) {
+                // 当前位置小于目标值 决策选出最大小于目标值的数
+                near = Math.max(near, matrix[row][col]);
+                // 计算出当前第几小 把小于当前值数加上
+                num += col + 1;
+                // 去大的可能位置
+                row++;
+            } else {
+                // 当前值大于目标值 只能去小的可能位置
+                col--;
+            }
+        }
+        return new Info(near, num);
+    }
+    
     
     public static void main(String[] args) {
         int RUNNING = -1 << COUNT_BITS;
@@ -126,9 +218,17 @@ public class KthSmallestElementInSortedMatrix {
         System.out.println(Integer.toBinaryString(3 << (Integer.SIZE - 3)));
         int[][] a = {{}};
         System.out.println(a.length);
-        int[][] matrix = {{1,5,9},{10,11,13},{12,13,15}};
-        int k = 8;
+        //int[][] matrix = {{1,5,9},{10,11,13},{12,13,15}};
+        int[][] matrix = {
+                {1,2,3,50},
+                {49,78,79,80},
+                {78,79,80,81},
+                {79,80,81,100}
+        };
+        int k = 1;
         KthSmallestElementInSortedMatrix kthSmallestElementInSortedMatrix = new KthSmallestElementInSortedMatrix();
         System.out.println(kthSmallestElementInSortedMatrix.kthSmallest(matrix, k));
+        System.out.println(kthSmallest2(matrix, k));
+        System.out.println(kthSmallest3(matrix, k));
     }
 }
