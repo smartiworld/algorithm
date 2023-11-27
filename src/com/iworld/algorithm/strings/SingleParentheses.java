23595d0bdc08d2292224b5809d1d2061a8a0bb2a
smartiworld <smartiworld@163.com>
2023-11-27 17:23:05 +0800

    3.1.2 验证字符串括号

--- src/com/iworld/algorithm/strings/SingleParentheses.java
+++ src/com/iworld/algorithm/strings/SingleParentheses.java
@@ -3,12 +3,17 @@ package com.iworld.algorithm.strings;
 /**
  * @author gq.cai
  * @version 1.0
- * @description 单一括号问题
+ * @description 3.1.2 单一括号问题
  * 17 经典题
  * @date 2022/7/11 18:37
  */
 public class SingleParentheses {
     
+    /**
+     * 验证括号字符串是否符合规则
+     * @param s
+     * @return
+     */
     public boolean isisValid(String s) {
         char[] chars = s.toCharArray();
         int count = 0;
@@ -18,6 +23,11 @@ public class SingleParentheses {
         return count == 0;
     }
     
+    /**
+     * 需要补几个括号 使得整体字符串括号有效
+     * @param s
+     * @return
+     */
     public int needParenthesesCount(String s) {
         char[] chars = s.toCharArray();
         int needCont = 0;
