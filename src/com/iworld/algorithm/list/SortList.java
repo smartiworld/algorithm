b2766a2de4aa1a113dbbee815e95839446634441
smartiworld <smartiworld@163.com>
2025-12-18 21:37:06 +0800

    链表排序

--- src/com/iworld/algorithm/list/SortList.java
+++ src/com/iworld/algorithm/list/SortList.java
@@ -306,6 +306,46 @@ public class SortList {
         return count;
     }
     
+    public ListNode sortList4(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pre = null;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
+        ListNode leftNode = sortList4(head);
+        ListNode rightNode = sortList4(slow);
+        return mergeTwoListAndOne(leftNode, rightNode);
+    }
+    
+    private ListNode mergeTwoListAndOne(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode cur = result;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return result.next;
+    }
+    
     public static void main(String[] args) {
         //[4,19,14,5,-3,1,8,5,11,15]
         ListNode l1 = new ListNode(4);
